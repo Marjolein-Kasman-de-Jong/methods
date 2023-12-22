8917b38c9dabd41128398db4a8c7ec9048631375
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2023-12-22 14:20:24 +0100

    Finished string-methods.js exercises

--- string-methods.js
+++ string-methods.js
@@ -3,8 +3,8 @@
 // ---- Verwachte uitkomst: 10
 
 const word = "zoetsappig";
-console.log(word.length);
 
+console.log(word.length);
 
 /* Opdracht 1 */
 //  Check of het woord "Banaan" langer is dan het woord "Aardappel" en log dit in de terminal.
@@ -13,7 +13,7 @@ console.log(word.length);
 const fruit = "Banaan";
 const vegetable = "Aardappel";
 
-
+console.log(fruit.length > vegetable.length);
 
 /* Opdracht 2 */
 //  Zorg ervoor dat de laatste letter van het woord in fruit en vegetable (hierboven) in de terminal gelogd wordt.
@@ -21,7 +21,7 @@ const vegetable = "Aardappel";
 // ---- Verwachte uitkomst voor fruit: "n"
 // ---- Verwachte uitkomst voor vegetable: "l"
 
-
+console.log(fruit.slice(-1), vegetable.slice(-1));
 
 /* Opdracht 3 */
 //  Haal de spaties aan het begin en eind van onderstaande zin weg en log dit in de terminal
@@ -29,7 +29,7 @@ const vegetable = "Aardappel";
 
 const userInput = "  De kat krabt de krullen van de trap ";
 
-
+console.log(userInput.trim());
 
 /* Opdracht 4 */
 // Check of het woord "Fantastisch" in onderstaande zin voorkomt en log dit in de terminal.
@@ -37,13 +37,13 @@ const userInput = "  De kat krabt de krullen van de trap ";
 
 const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch, nietwaar?"
 
-
+console.log(story.includes('Fantastisch'));
 
 /* Opdracht 5 */
 // Knip de vraag van bovenstaande tekst (story) af, zodat alleen het eerste gedeelte overblijft en log dit in de terminal.
 // ---- Verwachte uitkomst: "We vinden de behaalde resultaten werkelijk prachtig."
 
-
+console.log(story.slice(0, 52));
 
 /* Opdracht 6 */
 // Maak van onderstaand bedrag een Nederlands bedrag door het volgende te doen:
@@ -54,7 +54,4 @@ const story = "We vinden de behaalde resultaten werkelijk prachtig. Fantastisch,
 
 const price = "$35.59";
 
-
-
-
-
+console.log((price.replace("$", "€")).replace('.', ','));
